8f4975e42382f82330f3f1ac2d6454943e0058cc
Everton <thi.ger21@gmail.com>
2022-08-23 18:35:52 -0300

    Criação do Componente Typografy e os arquivos Heading e Paragraph com outro tipo de renderização.

---
+++ src/stories/Heading.stories.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import Heading from '../Components/Typografy/Heading';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default { 
+  title: 'Typografy/Heading',
+  component: Heading,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as ComponentMeta<typeof Heading>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof Heading> = (args) => <Heading {...args} />;
+
+export const Heading1 = Template.bind({})
+Heading1.args = {
+    level: 1,
+    children: 'Heading 1'
+}
+
+export const Heading2 = Template.bind({})
+Heading2.args = {
+    level: 1,
+    children: 'Heading 2'
+}
+
+export const Heading3 = Template.bind({})
+Heading1.args = {
+    level: 3,
+    children: 'Heading 3'
+}
---
+++ src/stories/Paragraph.stories.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import Paragraph from '../Components/Typografy/Paragraph';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default { 
+  title: 'Typografy/Paragraph',
+  component: Paragraph,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as ComponentMeta<typeof Paragraph>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof Paragraph> = (args) => <Paragraph {...args} />;
+
+export const Default = Template.bind({})
+Default.args = {
+  size: 'default',
+  children: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor rhoncus dolor purus non enim praesent elementum facilisis leo, vel'
+}
+
+export const Small = Template.bind({})
+Small.args = {
+  size: 'small',
+  children: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor rhoncus dolor purus non enim praesent elementum facilisis leo, vel'
+}
