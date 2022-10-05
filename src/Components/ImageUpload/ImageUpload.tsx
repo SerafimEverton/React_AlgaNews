ac0f6bf832b4546238d43191fb5c4a7df46acd5f
Everton <thi.ger21@gmail.com>
2022-10-04 21:02:57 -0300

    Finalizando ImageUpload; Criando Button para desfazer imagem

--- src/Components/ImageUpload/ImageUpload.tsx
+++ src/Components/ImageUpload/ImageUpload.tsx
@@ -1,5 +1,7 @@
 import { mdiUpload } from '@mdi/js'
 import Icon from '@mdi/react'
+import { ChangeEvent, useState } from 'react'
+import Button from '../Button/Button'
 import * as IU from './ImageUpload.styles'
 
 export interface ImageUploadProps {
@@ -8,6 +10,35 @@ export interface ImageUploadProps {
 }
 
 function ImageUpload (props: ImageUploadProps){
+    const [filePreview, setFilePreview] = useState<string | null>(null)
+
+    function handleChange (e: ChangeEvent<HTMLInputElement>){
+        const file = e.target.files![0]
+
+        if(file){
+            const reader = new FileReader()
+
+            reader.addEventListener('load', e=>{
+                setFilePreview(String(e.target?.result));
+            })
+
+            reader.readAsDataURL(file)
+        }
+
+    }
+
+    if(filePreview){
+        return <IU.ImagePreviewWrapper>
+            <IU.ImagePreview preview= {filePreview}>
+                 <Button 
+                 variant= {'primary'} 
+                 label={'Remover Imagem'} 
+                 onClick= {()=> setFilePreview(null)}
+                 /> 
+            </IU.ImagePreview>
+        </IU.ImagePreviewWrapper>
+    }
+
     return <IU.Wrapper>
         <IU.Label>
             <Icon
@@ -15,6 +46,10 @@ function ImageUpload (props: ImageUploadProps){
                 path={mdiUpload}
             />
             {props.label}
+            <IU.Input
+                type ='file'
+                onChange = {e=> handleChange}
+            />
         </IU.Label>
     </IU.Wrapper>
 
