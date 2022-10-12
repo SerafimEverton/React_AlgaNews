39f602124659472e212df2a8b92cee1857eb7680
Everton <thi.ger21@gmail.com>
2022-10-12 14:05:35 -0300

    Corrigindo erro do WordPriceCounter

--- src/Components/WordPriceCounter/WordPriceCounter.tsx
+++ src/Components/WordPriceCounter/WordPriceCounter.tsx
@@ -1,26 +1,23 @@
+import { useState } from 'react'
 import * as WPC from './WordPriceCounter.styles'
 
-export interface WordPriceCounterProps{
-wordsCount: number;
-pricePerWord: number;
-
+export interface WordPriceCounterProps {
+  wordsCount: number;
+  pricePerWord: number;
 }
 
-function WordPriceCounter (props: WordPriceCounterProps){
-    if (props.wordsCount < 0){
-        throw Error('A quantidade de palavras não pode ser menor que 0');
-    }
+function WordPriceCounter (props: WordPriceCounterProps) {
+  if (props.wordsCount < 0)
+    throw Error('A quantidade de palavras não pode ser negativa')
 
-    return <WPC.Wrapper>
-            <WPC.WordCounter>
-                { props.wordsCount} Palavras
-            </WPC.WordCounter>
-            <WPC.PricePreview>{(props.wordsCount * props.pricePerWord).toLocaleString('pt-br',{
-                style: 'currency',
-                maximumFractionDigits: 2
-            })}
-            </WPC.PricePreview>
-    </WPC.Wrapper>
+  return <WPC.Wrapper>
+    <WPC.WordCounter>{ props.wordsCount } palavras</WPC.WordCounter>
+    <WPC.PricePrivew>{(props.pricePerWord * props.wordsCount).toLocaleString('pt-br', {
+      style: 'currency',
+      currency: 'BRL',
+      maximumFractionDigits: 2
+    })}</WPC.PricePrivew>
+  </WPC.Wrapper>
 }
 
 export default WordPriceCounter
