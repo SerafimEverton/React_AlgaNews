fc3cc2c8f7dda9b000cc3fd7cf030f2d4070c387
Everton <thi.ger21@gmail.com>
2022-11-05 17:22:53 -0300

    Realizando atualizações e criando o componente ConfirmButton

--- src/App/Layouts/Default/Default.layout.tsx
+++ src/App/Layouts/Default/Default.layout.tsx
@@ -1,36 +1,39 @@
+import confirm from '../../../Core/Utils/ConfirmButton'
 import Logo from '../../Components/Logo'
 import NavBar from '../../Components/NavBar.Componets'
 import SessionController from '../../Components/SessionController'
 import * as DL from './Default.layout.styles'
-
 interface DefaultLayoutProps {
     children: React.ReactNode
-}
-
-function DefaultLayout (props: DefaultLayoutProps){
-
-return <DL.Wrapper>
-
-<DL.Header>
-    <Logo />
-</DL.Header>
-
-<DL.Main>
-    <DL.Navigation>
-        <NavBar />
-    </DL.Navigation>
-    <DL.FeaturedContent>
-        { props.children}
-    </DL.FeaturedContent>
-    <DL.Aside>
-        <SessionController 
-        name = "Everton Serafim"
-        description = "Estudante de TI FullStack"
-        />
-    </DL.Aside>
-</DL.Main>
-
-</DL.Wrapper>
-}
-
-export default DefaultLayout
+  }
+  
+  function DefaultLayout (props: DefaultLayoutProps) {
+    return <DL.Wrapper>
+      <DL.Header>
+        <Logo />
+      </DL.Header>
+      <DL.Main>
+        <DL.Navigation>
+          <NavBar />
+        </DL.Navigation>
+        <DL.FeaturedContent>
+          { props.children }
+        </DL.FeaturedContent>
+        <DL.Aside>
+          <SessionController
+            name="Daniel Bonifacio"
+            description="editor há 2 anos"
+            onLogout={() => {
+              confirm({
+                title: 'Voce quer delogar?',
+                onConfirm: () => window.alert('batata'),
+                onCancel: () => window.alert('laranja'),
+              })
+            }}
+          />
+        </DL.Aside>
+      </DL.Main>
+    </DL.Wrapper>
+  }
+  
+  export default DefaultLayout
