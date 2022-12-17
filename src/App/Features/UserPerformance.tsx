e26f4049f0d8e5250de2566eed79c6291058be58
Everton <thi.ger21@gmail.com>
2022-12-17 20:15:08 -0300

    Transformando os dados do ChartJs

--- src/App/Features/UserPerformance.tsx
+++ src/App/Features/UserPerformance.tsx
@@ -1,4 +1,8 @@
-import Chart from "../Components/Chart/Chart"
+import { useEffect } from "@storybook/addons"
+import { useState } from "react"
+import transformEditorMonthlyEarningsInfoChartJs from "../../Core/Utils/transformEditorMonthlyEarningsInfoChartJs"
+import MetricService from "../../SDK/services/Metric.service"
+import Chart, { ChartProps } from "../Components/Chart/Chart"
 
 const FAKE_DATA = {
   labels: ['Batata', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
@@ -6,27 +10,34 @@ const FAKE_DATA = {
     {
       label: 'Receitas',
       data: [500, 400, 600, 100, 800, 20, 123, 320, 120, 500, 434, 322],
-      fill: true,
-      backgroundColor: '#0099FF',
-      borderColor: '#0099FF',
-      borderWidth: 0.5,
+      
       yAxisID: 'cashflow',
     },
     {
       label: 'Despesas',
       data: [100, 200, 250, 500, 1000, 600, 123, 210, 344, 800, 123, 0],
-      fill: true,
-      backgroundColor: '#274060',
-      borderColor: '#274060',
-      borderWidth: 0.5,
+      
       yAxisID: 'cashflow',
     },
   ]
 }
 
-export default function UsePageTitle () {
+export default function UserPerformance() {
+
+  const [editorEarnings, setEditorEarnings] = useState<ChartProps['data']>()
+
+  useEffect(()=> {
+    MetricService
+    .getEditorMonthlyEarnings()
+    .then(transformEditorMonthlyEarningsInfoChartJs)
+    .then(setEditorEarnings)
+  }, [])
+
+  if(!editorEarnings)
+  return null
+
   return <Chart
     title="Média de performance nos últimos 12 meses"
-    data={FAKE_DATA}
+    data={editorEarnings}
   />
 }
