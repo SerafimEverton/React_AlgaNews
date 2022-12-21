260457fbf5b267808b90afb6653f7b58288aa680
Everton <thi.ger21@gmail.com>
2022-12-21 18:23:32 -0300

    Criando o Loading para indicar espera no processo

---
+++ src/App/Components/Loading/Loading.tsx
@@ -0,0 +1,17 @@
+import { LoadingWrapper } from "./loading.styles";
+
+export default function Loading() {
+
+return <LoadingWrapper>
+
+<div className="lds-ring">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+
+</LoadingWrapper>
+
+
+}
