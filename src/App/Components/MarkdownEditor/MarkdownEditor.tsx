c4ea4ff3c9e63382cd488cc693991aa41feeef95
Everton <thi.ger21@gmail.com>
2022-12-29 16:50:49 -0300

    Renderizando o Markdown, adicionando o PostPreview, criando o modal

--- src/App/Components/MarkdownEditor/MarkdownEditor.tsx
+++ src/App/Components/MarkdownEditor/MarkdownEditor.tsx
@@ -2,20 +2,27 @@ import MarkdownIt from 'markdown-it'
 import MdEditor, { Plugins } from 'react-markdown-editor-lite'
 import 'react-markdown-editor-lite/lib/index.css';
 
-MdEditor.unuse(Plugins.FontUnderline)// Para não utilizar o underline
+MdEditor.unuse(Plugins.FontUnderline)
 
 const parser = new MarkdownIt()
 
 export interface MarkdownEditorProps {
-
-  onChange?: (text: string)=> any
-
+  onChange?: (text: string) => any
+  value?: string;
+  readOnly?: boolean;
 }
 
 export default function MarkdownEditor (props: MarkdownEditorProps) {
   return <MdEditor
-    style={{ height: 300 }}
+    readOnly={props.readOnly}
+    style={{ height: props.readOnly ? 'auto' : 300 }}
+    value={props.value}
     renderHTML={text => parser.render(text)}
     onChange={({ text }) => props.onChange && props.onChange(text)}
+    view={props.readOnly ? {
+      menu: false,
+      md: false,
+      html: true
+    }: undefined}
   />
 }
--- src/App/Features/PostList.tsx
+++ src/App/Features/PostList.tsx
@@ -1,132 +1,156 @@
-import { mdiOpenInNew } from "@mdi/js"
-import Icon from "@mdi/react"
-import { format } from "date-fns"
-import { useEffect, useMemo, useState } from "react"
-import { Skeleton } from "react-loading-skeleton/dist/Skeleton"
-import { Column, usePagination, useTable } from "react-table"
-import withBoundary from "../../Core/hoc/withBoundary"
-import { Post } from "../../SDK/@types"
-import PostService from "../../SDK/services/Post.service"
-import Loading from "../Components/Loading"
-import Table from "../Components/Table/Table"
+import { mdiOpenInNew } from "@mdi/js";
+import Icon from "@mdi/react";
+import format from "date-fns/format";
+import parseISO from "date-fns/parseISO";
+import { useState } from "react";
+import { useEffect } from "react";
+import { useMemo } from "react";
+import Skeleton from "react-loading-skeleton";
+import { Column, usePagination, useTable } from "react-table";
+import modal from '../../Core/Utils/modal'
+import PostService from "../../SDK/services/Post.service";
+import Loading from "../Components/Loading";
+import Table from "../Components/Table/Table";
+import PostPreview from "./PostPreview";
+import {Post} from 
 
-function PostList () {
+export default function PostList() {
+  const [posts, setPosts] = useState<Post.Paginated>();
+  const [error, setError] = useState<Error>();
+  const [page, setPage] = useState(0);
+  const [loading, setLoading] = useState(false);
 
-  const [posts, setPosts] = useState<Post.Paginated>()
+  useEffect(() => {
+    setLoading(true);
+    PostService.getAllPosts({
+      page,
+      size: 7,
+      showAll: true,
+      sort: ["createdAt", "desc"],
+    })
+      .then(setPosts)
+      .catch((error) => setError(new Error(error.message)))
+      .then(() => {
+        setLoading(false);
+      });
+  }, [page]);
 
-  const [error, setError] = useState<Error>()
-
-  const [page, setPage] = useState(0)
-
-  const [loading, setLoading] = useState(false)
-
-
- useEffect(()=>{
-
-  setLoading(true)
-
-  PostService
-  .getAllPosts({
-    page,
-    size: 7,
-    showAll: true,
-    sort:['createdAt', 'desc']
-  })
-  .then(setPosts)
-  .catch(error => setError(new Error(error.message)))
-  .finally(()=>{setLoading(false)})
- }, [page])
-
-  if(error)
-    throw error
+  if (error) throw error;
 
   const columns = useMemo<Column<Post.Summary>[]>(
     () => [
       {
-        Header: '',
-        accessor: 'id', // accessor is the "key" in the data
-        Cell: () => <Icon path={mdiOpenInNew} size={'14px'} color={'#09f'} />
+        Header: "",
+        accessor: "id", // accessor is the "key" in the data
+        Cell: () => <Icon path={mdiOpenInNew} size={"14px"} color={"#09f"} />,
       },
       {
-        Header: () => <div style={{ textAlign: 'left' }}>Título</div>,
-        accessor: 'title',
+        Header: () => <div style={{ textAlign: "left" }}>Título</div>,
+        accessor: "title",
         width: 320,
-        Cell: (props) => <div style={{ textAlign: 'left', display: 'flex', gap: 8, alignItems: 'center' }}>
-          <img width={24} height={24} src={props.row.original.editor.avatarUrls.small} alt={props.row.original.editor.name}
-          title={props.row.original.editor.name}
-          />
-           {props.value}</div>
-      
+        Cell: (props) => (
+          <div
+            style={{
+              textAlign: "left",
+              display: "flex",
+              gap: 8,
+              alignItems: "center",
+            }}
+          >
+            <img
+              width={24}
+              height={24}
+              src={props.row.original.editor.avatarUrls.small}
+              alt={props.row.original.editor.name}
+              title={props.row.original.editor.name}
+            />
+            <a
+              href={`/posts/${props.row.original.id}`}
+              onClick={(e) => {
+                e.preventDefault();
+                modal({
+                  children: <PostPreview postId={props.row.original.id} />,
+                });
+              }}
+            >
+              {props.value}
+            </a>
+          </div>
+        ),
       },
       {
-        Header: () => <div style={{ textAlign: 'right' }}>Criação</div>,
-        accessor: 'createdAt',
-        Cell: (props) => <div style={{ textAlign: 'right', fontFamily: '"Roboto mono", monospace' }}
-        >
-         {format (new Date(props.value), 'dd/MM/yyyy - kk:mm')}
-
-</div>
+        Header: () => <div style={{ textAlign: "right" }}>Criação</div>,
+        accessor: "createdAt",
+        Cell: (props) => (
+          <div
+            style={{
+              textAlign: "right",
+              fontFamily: '"Roboto mono", monospace',
+            }}
+          >
+            {format(parseISO(props.value), "dd/MM/yyyy")}
+          </div>
+        ),
       },
       {
-        Header: () => <div style={{ textAlign: 'right' }}>Ultima atualização</div>,
-        accessor: 'updatedAt',
-        Cell: (props) => <div style={{ textAlign: 'right', fontFamily: '"Roboto mono", monospace' }}>
-          {format (new Date(props.value), 'dd/MM/yyyy - kk:mm')}
-
-        </div>
-        
-        },
-
-      
+        Header: () => (
+          <div style={{ textAlign: "right" }}>Última atualização</div>
+        ),
+        accessor: "updatedAt",
+        Cell: (props) => (
+          <div
+            style={{
+              textAlign: "right",
+              fontFamily: '"Roboto mono", monospace',
+            }}
+          >
+            {format(parseISO(props.value), "dd/MM/yyyy")}
+          </div>
+        ),
+      },
       {
         id: Math.random().toString(),
-        accessor: 'published',
-        Header: () => <div style={{ textAlign: 'right' }}>Ações</div>,
-        Cell: (props) => <div style={{ textAlign: 'right' }}>
-          {
-            props.value ? "Publicado" : "Privado"
-          }
-        </div>
+        accessor: "published",
+        Header: () => <div style={{ textAlign: "right" }}>Ações</div>,
+        Cell: (props) => (
+          <div style={{ textAlign: "right" }}>
+            {props.value ? "Publicado" : "Privado"}
+          </div>
+        ),
       },
     ],
     []
-  )
+  );
 
-  const instance = useTable<Post.Summary>({ 
-    data: posts?.content || [],
-    columns,
-    manualPagination: true,
-    initialState: {pageIndex: 0},
-    pageCount: posts?.totalPages
-  },
-  
+  const instance = useTable<Post.Summary>(
+    {
+      data: posts?.content || [],
+      columns,
+      manualPagination: true,
+      initialState: { pageIndex: 0 },
+      pageCount: posts?.totalPages,
+    },
     usePagination
-  
-  )
+  );
 
   if (!posts)
-    return <div>
-      <Skeleton height={32} />
-      <Skeleton height={40} />
-      <Skeleton height={40} />
-      <Skeleton height={40} />
-      <Skeleton height={40} />
-      <Skeleton height={40} />
-      <Skeleton height={40} />
-      <Skeleton height={40} />
-    </div>
+    return (
+      <div>
+        <Skeleton height={32} />
+        <Skeleton height={40} />
+        <Skeleton height={40} />
+        <Skeleton height={40} />
+        <Skeleton height={40} />
+        <Skeleton height={40} />
+        <Skeleton height={40} />
+        <Skeleton height={40} />
+      </div>
+    );
 
-  return <> 
-  
-  <Loading show={loading}/>
-  
-  <Table
-    instance={instance}
-    onPaginate={setPage}
-
-  />
-
-  </>
+  return (
+    <>
+      <Loading show={loading} />
+      <Table instance={instance} onPaginate={setPage} />
+    </>
+  );
 }
-
-export default withBoundary(PostList, 'Post List')
