17e00797b7f9c40ac92f0a2cd7c8544216682482
Everton <thi.ger21@gmail.com>
2022-12-29 19:57:36 -0300

    Instalando o CodeSnippet n

--- src/App/Components/MarkdownEditor/MarkdownEditor.tsx
+++ src/App/Components/MarkdownEditor/MarkdownEditor.tsx
@@ -6,13 +6,33 @@ MdEditor.unuse(Plugins.FontUnderline)
 
 const parser = new MarkdownIt()
 
+/*-*-*-*-*-*-*-*-*-*-*-*CODE SNIPPET*-*-*-*-*-*-*-*-*-*-*-*/
+
+const defaultRender = parser.renderer.rules.link_open ||
+  function (tokens: any, idx: any, options: any, env: any, self: any) {
+    return self.renderToken(tokens, idx, options);
+  };
+
+parser.renderer.rules.link_open = function (tokens, idx, options, env, self) {
+  var aIndex = tokens[idx].attrIndex('target');
+  if (aIndex < 0) {
+    tokens[idx].attrPush(['target', '_blank']);
+  } else {
+    //@ts-ignore
+    tokens[idx].attrs[aIndex][1] = '_blank';
+  }
+  return defaultRender(tokens, idx, options, env, self);
+};
+
+/*-*-*-*-*-*-*-*-*-*-*-*CODE SNIPPET*-*-*-*-*-*-*-*-*-*-*-*/
+
 export interface MarkdownEditorProps {
   onChange?: (text: string) => any
   value?: string;
   readOnly?: boolean;
 }
 
-export default function MarkdownEditor (props: MarkdownEditorProps) {
+export default function MarkdownEditor(props: MarkdownEditorProps) {
   return <MdEditor
     readOnly={props.readOnly}
     style={{ height: props.readOnly ? 'auto' : 300 }}
@@ -23,6 +43,6 @@ export default function MarkdownEditor (props: MarkdownEditorProps) {
       menu: false,
       md: false,
       html: true
-    }: undefined}
+    } : undefined}
   />
 }
